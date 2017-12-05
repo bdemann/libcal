49da88b5eccdca9f23507fee45f0d2a31ab59721
Benjamin DeMann <benjamin.demann@gmail.com>
2017-12-04 22:26:00 -0700

    added some more layout

--- src/components/lc-calendar-column/lc-calendar-column.ts
+++ src/components/lc-calendar-column/lc-calendar-column.ts
@@ -1,5 +1,9 @@
 class LCCalendarColumn extends Polymer.Element {
 	static get is(){ return 'lc-calendar-column'; }
+
+	report () {
+		console.log('we are hovering over the thing')
+	}
 }
 
 window.customElements.define(LCCalendarColumn.is, LCCalendarColumn)
